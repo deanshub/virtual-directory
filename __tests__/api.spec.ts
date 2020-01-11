60bf6deac159267be29bcadb6593ad884a1dd14e
Dean Shub <deanshub@gmail.com>
2020-01-11 20:38:13 +0200

    adding tests

--- __tests__/api.spec.ts
+++
@@ -1,7 +0,0 @@
-import virtualDirectory from '../index'
-
-process.on('unhandledRejection',(e) => {
-    console.error(e)
-    throw e
-})
-virtualDirectory('__tests__/srcProject','__tests__/destProject',['dir1/dir1.1'])
--- __tests__/e2e.spec.ts
+++ __tests__/e2e.spec.ts
@@ -1,12 +1,59 @@
 import virtualDirectory from '../index'
+import fs from 'fs-extra'
+import path from 'path'
 
-describe('e2e', () => {
-    test('linking without exclusions', () => {
-        return virtualDirectory('./srcProject', './destProject')
-    })
+const join = path.join
+const SRC = './__tests__/srcProject'
+const DEST = './__tests__/destProject'
+
+async function isSymlink(path: string) {
+  const stat = await fs.lstat(path)
+  return stat.isSymbolicLink()
+}
+async function fileExists(path: string) {
+  try {
+    const stat = await fs.lstat(path)
+    return stat.isFile()
+  } catch (err) {
+    return false
+  }
+}
+async function dirExists(path: string) {
+  try {
+    const stat = await fs.lstat(path)
+    return stat.isDirectory()
+  } catch (err) {
+    return false
+  }
+}
+
+describe('node api', () => {
+  test('linking', async () => {
+    await virtualDirectory(SRC, DEST)
+    expect(await isSymlink(DEST)).toBe(true)
+  })
+
+  test('linking with file exclusion', async () => {
+    await virtualDirectory(SRC, DEST, ['file1'])
+    expect(await isSymlink(DEST)).toBe(false)
+    expect(await fileExists(join(DEST, 'file1'))).toBe(false)
+  })
+
+  test('linking with dir exclusion', async () => {
+    await virtualDirectory(SRC, DEST, ['dir1'])
+    expect(await isSymlink(DEST)).toBe(false)
+    expect(await dirExists(join(DEST, 'dir1'))).toBe(false)
+  })
+  test('linking with all exclusions', async () => {
+    await virtualDirectory(SRC, DEST, ['dir1/dir1.1', 'dir1/file1.1'])
+    expect(await isSymlink(DEST)).toBe(false)
+    expect(await isSymlink(join(DEST, 'file1'))).toBe(true)
+    expect(await dirExists(join(DEST, 'dir1/dir1.1'))).toBe(false)
+    expect(await dirExists(join(DEST, 'dir1/file1.1'))).toBe(false)
+  })
 })
 
-    /*
+/*
 const exclusions=['node_modules/a1/a1.b1/a1.b1.c1', 'node_modules/a1/a1.b2', 'node_modules/a2']
 describe('exclusions', ()=>{
     test('sorting', ()=>{
